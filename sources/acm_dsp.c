3863e4db45a6e033684f2ccb8c4aea54deb80a48
chenfyrm <491700436@qq.com>
2019-05-21 18:33:09 +0800

    11

--- sources/acm_dsp.c
+++ sources/acm_dsp.c
@@ -735,10 +735,10 @@ cfloat32 CPLXMULT(cfloat32 Z1, cfloat32 Z2) {
 	return Z;
 }
 
-cfloat32 CPLXMULT_SHFT(cfloat32 Z1, cfloat32 Z2, int32 m) {
-	cfloat32 Z = { 0, 0 };
-	return Z;
-}
+//cfloat32 CPLXMULT_SHFT(cfloat32 Z1, cfloat32 Z2, int32 m) {
+//	cfloat32 Z = { 0, 0 };
+//	return Z;
+//}
 
 float32 CPLXNORM(cfloat32 Z) {
 	return Z.re * Z.re + Z.im * Z.im;
@@ -751,15 +751,15 @@ cfloat32 CPLXSCA(cfloat32 Z1, float32 a) {
 	return Z;
 }
 
-cfloat32 CPLXSCA_SHFT(cfloat32 Z1, float32 a, int32 m) {
-	cfloat32 Z = { 0, 0 };
-	return Z;
-}
+//cfloat32 CPLXSCA_SHFT(cfloat32 Z1, float32 a, int32 m) {
+//	cfloat32 Z = { 0, 0 };
+//	return Z;
+//}
 
-cfloat32 CPLXSHFT(cfloat32 Z1, int32 m) {
-	cfloat32 Z = { 0, 0 };
-	return Z;
-}
+//cfloat32 CPLXSHFT(cfloat32 Z1, int32 m) {
+//	cfloat32 Z = { 0, 0 };
+//	return Z;
+//}
 
 cfloat32 CPLXSUB(cfloat32 Z1, cfloat32 Z2) {
 	cfloat32 Z;
@@ -775,20 +775,27 @@ cfloat32 CPLXADD(cfloat32 Z1, cfloat32 Z2) {
 	return Z;
 }
 
-cfloat32 _PREVCPLX(cfloat32 Z) {
+//cfloat32 _PREVCPLX(cfloat32 Z) {
+//
+//	return Z;
+//}
 
-	return Z;
-}
-
-cfloat32 CPLXDIVSCA(cfloat32 Z1, float32 F, int32 m) {
-	cfloat32 Z = { 0, 0 };
-	return Z;
-}
+//cfloat32 CPLXDIVSCA(cfloat32 Z1, float32 F, int32 m) {
+//	cfloat32 Z = { 0, 0 };
+//	return Z;
+//}
 
 cfloat32 CPLXDIV(cfloat32 Z1, cfloat32 Z2) {
+	float32 tmp;
 	cfloat32 Z;
-	Z.re = (Z1.re * Z2.re + Z1.im * Z2.im) / (Z2.re * Z2.re + Z2.im * Z2.im);
-	Z.im = (-Z1.re * Z2.im + Z1.im * Z2.re) / (Z2.re * Z2.re + Z2.im * Z2.im);
+	tmp = Z2.re * Z2.re + Z2.im * Z2.im;
+	if (tmp == 0) {
+		Z.re = 0.0;
+		Z.im = 0.0;
+	} else {
+		Z.re = (Z1.re * Z2.re + Z1.im * Z2.im) / tmp;
+		Z.im = (-Z1.re * Z2.im + Z1.im * Z2.re) / tmp;
+	}
 	return Z;
 }
 
@@ -820,12 +827,6 @@ cfloat32 POL2CPLX(float32 r, float32 fi) {
 
 /**/
 void SOGIOSGFLL(TYPE_SOGIOSGMA *data) {
-
-//	data->Ts = 1.0/2700.0;
-//	data->w0 = 100*3.1415926;
-//	data->K = sqrt(2);
-//	data->Ki = 10000;
-
 	/**/
 	data->a = data->Ts * data->w / 2.0 + 2.0 / data->Ts / data->w;
 	data->b = data->Ts * data->w / 2.0 - 2.0 / data->Ts / data->w;
